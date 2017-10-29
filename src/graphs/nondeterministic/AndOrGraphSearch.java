ca606cd0a7329113d438b9273ff75ba9a0049bff
viktorkuznietsov1986 <victor.kuzn@gmail.com>
2017-10-28 20:57:29 -0400

    intermediate changes to AndOrGraphSearch

--- src/graphs/nondeterministic/AndOrGraphSearch.java
+++ src/graphs/nondeterministic/AndOrGraphSearch.java
@@ -4,24 +4,33 @@ import graphs.Action;
 import graphs.FailureException;
 import graphs.State;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Represents the And-Or-Graph search algorithm for non-deterministic environments.
  */
 public class AndOrGraphSearch extends Search {
 
+    private Node _plan;
 
+    /**
+     * Gets the conditional plan to reach every state from start state.
+     * @return the conditional plan to reach every state from start state.
+     */
+    public Node getPlan() {
+        return _plan;
+    }
 
 
     public AndOrGraphSearch(Problem problem, State start) throws FailureException {
-        OrSearch(start, problem, new List<Node>());
+        Map<State, Node> result = OrSearch(start, problem, new LinkedList<>());
+
+        if (!result.isEmpty()) {
+
+        }
     }
 
-    private Map<Action, Node> OrSearch(State state, Problem problem, List<Node> path) throws FailureException {
+    private Map<State, Node> OrSearch(State state, Problem problem, List<Node> path) throws FailureException {
         if (state.isGoal()) {
             return new HashMap<>();
         }
@@ -31,14 +40,35 @@ public class AndOrGraphSearch extends Search {
 
 
         for (Action a : problem.getActions(state)) {
-            Map<State, Node> plan = AndSearch(problem.getResult(state, a), problem, path);
+            try {
+                Map<State, Node> plan = AndSearch(problem.getResult(state, a), problem, path);
+                return plan;
+            }
+            catch (FailureException e) {
+                // todo: do nothing as there should be at least one path; if no correct path, it'll throw a new FailureException.
+            }
+
         }
+
+        throw new FailureException();
 
     }
 
     private Map<State, Node> AndSearch(List<State> states, Problem problem, List<Node> path) throws FailureException {
+        Map<State, Node> result = new HashMap<>();
+
         for (State s : states) {
-            Map<Action, Node> plan = OrSearch(s, problem, path);
+            try {
+                Map<State, Node> plan = OrSearch(s, problem, path);
+
+                result.putAll(plan);
+            }
+            catch (FailureException e) {
+                throw e;
+            }
+
         }
+
+        return result;
     }
 }
